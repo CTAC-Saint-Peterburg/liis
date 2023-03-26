0b47d0a8fa0f728893eec38ad894bed49a9f4e6e
Стас <seleznev-00@mail.ru>
2023-03-26 13:46:32 +0300

    fix: url

--- src/store/urlforfetch/urlforfetchSlice.js
+++ src/store/urlforfetch/urlforfetchSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const urlforfetchSlice = createSlice({
   name: "urlforfetch",
   initialState: {
-    url: "http://engine.hotellook.com/api/v2/cache.json?location=Moscow&currency=rub&checkIn=2023-03-25&checkOut=2023-03-26&limit=10",
+    url: `http://engine.hotellook.com/api/v2/cache.json?location=Moscow&currency=rub&checkIn=2023-03-${new Date().getDate()}&checkOut=2023-03-${new Date().getDate()}&limit=10`,
   },
   reducers: {
     change: (state, action) => {
